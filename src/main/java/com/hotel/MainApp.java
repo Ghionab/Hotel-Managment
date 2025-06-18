f7dda29a3bdb3a5971b525e50f2ba4c103216256
Ghionab <abrahammulualem3@gmail.com>
2025-06-18 21:49:49 +0300

    Update MainApp.java

--- src/main/java/com/hotel/MainApp.java
+++ src/main/java/com/hotel/MainApp.java
@@ -14,7 +14,7 @@ import com.hotel.controllers.MainController;
 
 public class MainApp extends Application {
 
-    private static Stage primaryStage; // Keep a reference to the primary stage
+    private static Stage primaryStage; // Keep a reference to the
 
     @Override
     public void start(Stage stage) throws IOException {
