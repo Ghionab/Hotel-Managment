ca226f5d5e8bada5ad2651aea784d5be270027c6
Ghionab <abrahammulualem3@gmail.com>
2025-06-19 23:31:44 +0300

    Update MainApp.java

--- src/main/java/com/hotel/MainApp.java
+++ src/main/java/com/hotel/MainApp.java
@@ -7,8 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
-
-
 import com.hotel.controllers.MainController;
 
 public class MainApp extends Application {
